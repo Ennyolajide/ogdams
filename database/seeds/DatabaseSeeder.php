20bc92a370d3a9dd1664ad2d245e1d8738c73a10
Eniseyin Olajide <eniseyinolajde@gmail.com>
2019-09-19 15:37:25 +0100

    2nd release

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //Voucher Seeder
-        factory(App\Voucher::class, 20)->create();
+        //factory(App\Voucher::class, 20)->create();
 
         //Message Seeder
-        factory(App\Message::class, 20)->create();
+        //factory(App\Message::class, 20)->create();
 
         //Tansaction Seeder
-        factory(App\Transaction::class, 5)->create();
+        //factory(App\Transaction::class, 5)->create();
 
         $this->call(UsersTableSeeder::class);
         $this->call(BanksTableSeeder::class);
-        factory(App\Bank::class, 5)->create();
+        //factory(App\Bank::class, 5)->create();
         $this->call(BitcoinsTableSeeder::class); //First time Run
         $this->call(ChargesTableSeeder::class);
         $this->call(DataPlansTableSeeder::class); //First time Run
