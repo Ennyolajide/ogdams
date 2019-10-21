a26464bc5eb8f41f3d165a2b81ff1d48a835a9a9
Eniseyin Olajide <eniseyinolajde@gmail.com>
2019-10-21 11:10:41 +0100

    constants.php update

--- config/constants.php
+++ config/constants.php
@@ -18,6 +18,10 @@ return [
         'googleMap' => 'https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d7916.862982549558!2d3.4318005225864616!3d7.1915070292316345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x103a363bcdd152df%3A0xf3ab70233638c6ec!2sCamp%2C+Abeokuta!3m2!1d7.194497999999999!2d3.4359531!5e0!3m2!1sen!2sng!4v1564561313169!5m2!1sen!2sng',
         'about' => '©2019 All Rights Reserved. Ogams Techechnogies is a business solution website. Privacy and Terms',
 
+        'sms' => [
+            'sender' => env('SITE_SMS_SENDER_ID'),
+        ],
+
         'emails' => [
             'sender' => [
                 'name' => env('SITE_EMAIL_SENDER'),
@@ -70,7 +74,7 @@ return [
     'bonuses' => [
         'referral' => 150,
     ],
-
+    env('SMARTSMSSOLUTION_TOKEN')
 
 
 
